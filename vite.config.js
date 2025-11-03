1c1388aa6ff709b785446680baa431445a666ad9
BlackHorse <blackhorse@BlackHorses-MacBook-Air.local>
2025-11-03 16:52:20 -0500

    cleaned up starter&build=single script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.tsx"),
+      name: "AgiloWidget",
+      fileName: () => "widget.js",
+      formats: ["iife"], // single runnable script
+    },
+  },
+});
